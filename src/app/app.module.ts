8e12025a519fb7244f073ae3723c4018c3ec5c5e
Ahmed Salah <a.salah.b@gmail.com>
2016-12-20 21:12:23 +0200

    App Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import {FirebaseService} from "./services/firebase.service";
+
+export const firebaseConfig = {
+  apiKey: ' AIzaSyDhs8WljhFI2ohX7LlxBasXvBUwAdnglog',
+  authDomain: 'business-contacts-508cd.firebaseapp.com',
+  databaseURL: 'https://business-contacts-508cd.firebaseio.com',
+  storageBucket: 'business-contacts-508cd.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +21,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
